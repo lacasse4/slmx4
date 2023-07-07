71694d36a364f02fa61ac025044d3773626086c6
Vincent Lacasse <lacasse4@yahoo.com>
2023-07-07 19:05:43 -0400

    fftwrapper.c to be debugged

--- firstreflex/src/fftwrapper.c
+++ firstreflex/src/fftwrapper.c
@@ -8,16 +8,14 @@
 
 #include "fftwrapper.h"
 
-    fftwf_complex *in;
-    fftwf_complex *out;
-    fftwf_plan p;
+fftwf_plan p;
 
 struct fft_wrapper
 {
     int size;
     fftwf_complex *in;
     fftwf_complex *out;
-    fftwf_plan p;
+    fftwf_plan plan;
 };
 
 
@@ -29,61 +27,40 @@ fft_wrapper_t* fft_init(int size)
     }
 
     fft_wrapper->size = size;
-    in   = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * size);
-    out  = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * size);
-    p    = fftwf_plan_dft_1d(size, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-    // fft_wrapper->in   = in;
-    // fft_wrapper->out  = out;
-    // fft_wrapper->p    = p;
-    // fft_wrapper->in   = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * size);
-    // fft_wrapper->out  = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * size);
-    // fft_wrapper->p    = fftwf_plan_dft_1d(size, fft_wrapper->in, fft_wrapper->out, FFTW_FORWARD, FFTW_ESTIMATE);
-    // if (fft_wrapper->in == NULL || fft_wrapper->out == NULL || fft_wrapper->p == NULL) {
-    //     fft_release(fft_wrapper);
-    //     return NULL;
-    // }
+    fft_wrapper->in   = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * size);
+    fft_wrapper->out  = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * size);
+    fft_wrapper->plan = fftwf_plan_dft_1d(size, fft_wrapper->in, fft_wrapper->out, FFTW_FORWARD, FFTW_ESTIMATE);
+//    fft_wrapper->p    = p;
+//    p = fft_wrapper->p;
+
+    if (fft_wrapper->in == NULL || fft_wrapper->out == NULL || fft_wrapper->plan == NULL) {
+        fft_release(fft_wrapper);
+        return NULL;
+    }
 
     return fft_wrapper;
 }
 
 void fft_release(fft_wrapper_t* fft_wrapper)
 {
-    // in  = fft_wrapper->in;
-    // out = fft_wrapper->out;
     // p   = fft_wrapper->p;
-    fftwf_destroy_plan(p);
-    fftwf_free(out);
-    fftwf_free(in); 
-    // fftwf_destroy_plan(fft_wrapper->p);
-    // fftwf_free(fft_wrapper->out);
-    // fftwf_free(fft_wrapper->in); 
+    fftwf_destroy_plan(fft_wrapper->plan);
+    fftwf_free(fft_wrapper->out);
+    fftwf_free(fft_wrapper->in); 
     free(fft_wrapper);
 }
 
 void fft_spectrum(fft_wrapper_t* fft_wrapper, float* signal, float* spectrum)
 {
-    // in  = fft_wrapper->in;
-    // out = fft_wrapper->out;
     // p   = fft_wrapper->p;
 
     for (int i = 0; i < fft_wrapper->size; i++) {
-        in[i] = signal[i] + I * 0.0;
+        fft_wrapper->in[i] = signal[i] + I * 0.0;
     }
 
-    fftwf_execute(p);
+    fftwf_execute(fft_wrapper->plan);
 
     for (int i = 0; i < fft_wrapper->size/2+1; i++) {
-        spectrum[i] = cabsf(out[i]);
+        spectrum[i] = cabsf(fft_wrapper->out[i]);
     }
-
-    // for (int i = 0; i < fft_wrapper->size; i++) {
-    //     fft_wrapper->in[i] = signal[i] + I * 0.0;
-    // }
-
-    // fftwf_execute(fft_wrapper->p);
-
-    // for (int i = 0; i < fft_wrapper->size/2+1; i++) {
-    //     spectrum[i] = cabsf(fft_wrapper->out[i]);
-    // }
-
 }
